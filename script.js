aa38e9bc8e6371d7d72de0f4121688e35734f711
AntonioRosales1 <elrluis213@gmail.com>
2023-01-24 17:07:14 -0800

    added API

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log(`hello`);
+const verse = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "52fddf5b96msh707bfb679e0bb8fp1cc282jsn26b510fbe64f",
+    "X-RapidAPI-Host": "uncovered-treasure-v1.p.rapidapi.com",
+  },
+};
+
+fetch("https://uncovered-treasure-v1.p.rapidapi.com/verse/psalms%2010:5", verse)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
