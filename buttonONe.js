6c91c9316b8eaac2336cd5919546bca46fd408db
AntonioRosales1 <elrluis213@gmail.com>
2023-01-27 22:40:58 -0800

    arrays and buttons function starting

--- buttonONe.js
+++ buttonONe.js
@@ -2,10 +2,11 @@ const clickMe = document.getElementById(`verse1`);
 let verseImg1 = document.getElementById(`verseImg1`);
 let verse1Change = document.getElementById("verse1Change");
 let firstVerse = document.getElementById(`firstVerse`);
+let verse1 = document.getElementById(`verse1`);
 
 var imgArray = [
   "./img/father verse.webp",
-  "img/img1.jpg",
+
   "./img/Mother verse.jpeg",
   "./img/Prayer for son.webp",
   "./img/SIster verse.jpg",
@@ -23,6 +24,8 @@ clickMe.addEventListener(`click`, function (e) {
   } else {
     verseImg1.setAttribute(`src`, "/img/Mother verse.jpeg");
     h1.innerText = ` Blessed you are to have you mother.`;
-    clickMe.remove();
+    clickMe.style.backgroundColor = `#E89B9B `;
+    clickMe.innerText = `Thanks for viewing. You are out of clicks for the day. Come back tomorrow!`;
+    verse1.style.transform = false;
   }
 });
