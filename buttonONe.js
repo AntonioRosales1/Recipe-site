7ccd8440b801d152afa2c94fca601c745ecc02c2
AntonioRosales1 <elrluis213@gmail.com>
2023-05-21 22:29:00 -0700

    styling added

--- buttonONe.js
+++ buttonONe.js
@@ -23,9 +23,21 @@ clickMe.addEventListener(`click`, function (e) {
     h1.innerText = `A son is everything in life.`;
   } else {
     verseImg1.setAttribute(`src`, "/img/Mother verse.jpeg");
-    h1.innerText = ` Blessed you are to have you mother.`;
+    h1.innerText = ` Blessed you are to have your mother.`;
     clickMe.style.backgroundColor = `#E89B9B `;
     clickMe.innerText = `Thanks for viewing. You are out of clicks for the day. Come back tomorrow!`;
     verse1.style.transform = false;
   }
 });
+
+// clickMe.addEventListener("click", function (e) {
+//   for (i = 0; i < imgArray.length; i++) {
+//     if (imgArray == clickMe) {
+//       verseImg1.setAttribute = i;
+//     }
+//   }
+//   e();
+// });
+
+// verseImg1.src = imgArray[3];
+// for (i = 0; i < imgArray.length; i++){
--- quoteArray.js
+++ quoteArray.js
@@ -1,26 +1,40 @@
+let about = document.getElementById(`about`);
+const verseArraybtn = document.getElementById(`verse-Array-btn`);
+
 var quoteArray = [
   `John 16:33
-  
+
   "In the world you will have tribulation. But take heart; I have overcome the world."
   `,
   `Isaiah 41:10 (NIV)
-  
+
   "So do not fear, for I am with you; do not be dismayed, for I am your God. I will strengthen you and help you; I will uphold you with my righteous right hand."`,
   `Philippians 4:6–7 (NIV)
-  
-  Do not be anxious about anything, but in every situation, by prayer and petition, with thanksgiving, present your requests to God. And the peace of God, which 
+
+  Do not be anxious about anything, but in every situation, by prayer and petition, with thanksgiving, present your requests to God. And the peace of God, which
   transcends all understanding, will guard your hearts and your minds in Christ Jesus.`,
   `Psalm 34:4–5, 8
-  
+
   I sought the LORD, and He answered me and delivered me from all my fears.
-   Those who look to Him are radiant, and their faces shall never be ashamed. Oh, taste and see that the LORD is good! Blessed is the man who 
+   Those who look to Him are radiant, and their faces shall never be ashamed. Oh, taste and see that the LORD is good! Blessed is the man who
    takes refuge in Him!`,
   `Romans 8:28
-  
+
   And we know that for those who love God all things work together for good, for those who are called according to His purpose.`,
   `Joshua 1:9
-  
+
   "Have I not commanded you? Be strong and courageous. Do not be frightened, and do not be dismayed, for the LORD your God is with you wherever you go."`,
 ];
-console.log(quoteArray[0]);
-console.log(imgArray[0]);
+
+verseArraybtn.addEventListener(`click`, function (e) {
+  about.innerHTML = `You changed me! Now lets make that array work`;
+});
+
+// verseArraybtn.addEventListener("click", function (e) {
+//   for (i = 0; i < verseArraybtn.length; i++) {
+//     if (verseArraybtn == clickMe) {
+//       verseImg1.setAttribute = i;
+//     }
+//   }
+//   e();
+// });
